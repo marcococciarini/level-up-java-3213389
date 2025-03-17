b231346212681a2c3434684cfa161713400787c6
marcococciarini <156895913+marcococciarini@users.noreply.github.com>
2025-03-17 14:35:00 +0000

    done 100days from today

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,24 @@
 package com.linkedin.javacodechallenges;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+//import java.util.Date;
+
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
 
+    public static LocalDate getDateFromToday(LocalDate from, int numberOfDaysFromToday){
+        //LocalDate today =  LocalDate.now(ZoneId.of("Europe/Rome"));
+        return from.plusDays(numberOfDaysFromToday);
+    }
+
     public static void main( String[] args )
     {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+        LocalDate today = LocalDate.now(ZoneId.of("Europe/Rome"));
+
+        System.out.println("100 days from now is... " +
+            getDateFromToday(today, 100));
     }
 }
