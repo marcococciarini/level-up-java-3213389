f2a5338bf7a4eb710b316b63ecce280a375f2b45
marcococciarini <156895913+marcococciarini@users.noreply.github.com>
2025-03-17 12:23:42 +0000

    done Person Exercise

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person p1 = new Person("Marco", "Cocciarini", 43);
+        Person p2 = new Person("Simona", "Orselli", 45);
+        Person p3 = new Person("Gabriele", "Cappella", 53);
+
+        p1.introducePerson();
+        p2.introducePerson();
+        p3.introducePerson();
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,42 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
   
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;    
+  }
+
+  public void introducePerson(){
+    System.out.println("Hi! I'm " + this.lastName + " " + this.firstName + " and I'm " + this.age + " years old. Nice to meet you!");
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public void setAge(int age){
+    this.age = age;
+  }
+
+  public String getFirstName(){
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName){
+    this.firstName = firstName;
+  }
+
+  public String getLastName(){
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName  = lastName;
+  }
 }
