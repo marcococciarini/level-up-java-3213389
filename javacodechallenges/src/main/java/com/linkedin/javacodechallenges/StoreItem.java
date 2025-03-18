6b0a4dce761c0fa9be4a9b000d5fb9c4c4d6441b
marcococciarini <156895913+marcococciarini@users.noreply.github.com>
2025-03-18 16:13:02 +0000

    modifiche a esercizio storeitem - soluzione proposta dal corso aggiunta come commenti

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
@@ -20,7 +20,15 @@ public class StoreItem {
     // TODO: Implement
     Optional<StoreItem> minItem = items.stream().min( (item1,item2) -> Double.compare(item1.retailPrice - (item1.retailPrice* item1.discount), 
                                                         item2.retailPrice - (item2.retailPrice* item2.discount) ));
+    
+    // SOLUZIONE ESERCIZIO CORSO :
+    // return items.stream().min(Comparator.comparing(StoreItem::calculateCurrentPrice));
     return minItem;
+  }
+
+  // SOLUZIONE ESERCIZIO CORSO :
+  private double calculateCurrentPrice(){
+    return this.retailPrice - (this.retailPrice * this.discount);
   }
 
   @Override
