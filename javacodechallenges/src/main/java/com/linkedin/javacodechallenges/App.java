64bf596becf17ec4b2752119841bfb7a351ea813
marcococciarini <156895913+marcococciarini@users.noreply.github.com>
2025-03-18 16:47:07 +0000

    esercizio calcolo media degli investimenti

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,10 +1,26 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.List;
+import java.util.Optional;
 
 public class App {
     public static double calculateAverageChangeInvested(List<Double> purchases) {
-        return 0;
+        //return 0;
+
+        //Optional<Double> result = purchases.stream().reduce(Double::sum);
+        //Double result = purchases.stream().reduce(0.0,
+        //        (a, b) -> a + (Math.ceil(b) - b));
+
+        Double result = purchases.stream().mapToDouble(x -> (Math.ceil(x) - x)).average().orElse(0);
+
+        /* 
+        if (result.isPresent()){
+            return result.get();
+        } else{
+            return 0;
+        }
+        */
+        return result;
     }
 
     public static void main(String[] args) {
